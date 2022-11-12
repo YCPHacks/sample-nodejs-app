b96ff475def490ac2ed0f8591a4f3356c253fc38
Ralph Greaves <rgreaves@ycp.edu>
2022-11-12 14:17:01 -0500

    Update routes.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const users = require('./users.js');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/users', users);
 
 module.exports = router;
