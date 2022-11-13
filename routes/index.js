f21ea35f6e0540153a6665d0e9bc11c4450fca55
Ralph Greaves <rgreaves@ycp.edu>
2022-11-12 21:56:23 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const auth = require('express-openid-connect').auth();
+const { auth } = require('express-openid-connect');
 
 const users = require('./users.js');
 
