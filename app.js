b93fc1116a9b63d8b6a9db3656d0530b45058c9c
Ralph Greaves <rgreaves@ycp.edu>
2022-11-17 16:01:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).send(err.message);
+  res.status(err.status ?? 500).send(err.message);
 });
 
 module.exports = app;
