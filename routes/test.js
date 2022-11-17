c18581c1821fef23d0699b632d5deceb81ad418c
Ralph Greaves <rgreaves@ycp.edu>
2022-11-17 16:51:30 -0500

    Update test.js

--- routes/test.js
+++ routes/test.js
@@ -1,35 +1,13 @@
-/*
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
-*/
-
 const express = require('express');
 
 const { auth, requiresAuth } = require('express-openid-connect');
 
 const router = express.Router();
 
-/*
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Auth0 Webapp sample Nodejs',
-    isAuthenticated: req.oidc.isAuthenticated()
-  });
-});
-*/
-
 router.use(
   auth({
     authorizationParams: {
       response_type: 'code',
-      audience: process.env.AUDIENCE,
       scope: 'openid profile email read:messages offline_access'
     }
   })
